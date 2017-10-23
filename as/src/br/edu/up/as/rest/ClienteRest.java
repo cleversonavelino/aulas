80bfaf0f4179cb3ca18c5a45cf75a9e119ec7be1
CLEVERSONAVELINOFerr <CLEVERSONAVELINOFerr@9.80.208.67>
2017-10-23 20:07:31 -0200

    inclusão do serviço rest.

---
+++ as/src/br/edu/up/as/rest/ClienteRest.java
@@ -0,0 +1,45 @@
+package br.edu.up.as.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.up.as.dao.ClienteDao;
+import br.edu.up.as.entidade.Cliente;
+import br.edu.up.as.service.ClienteService;
+import br.edu.up.as.service.ServiceException;
+
+@Path("/detalhecliente")
+public class ClienteRest {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Cliente> listarClientes() {
+		List<Cliente> clientes = new ClienteDao().listar();
+		return new ArrayList<>(clientes);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void cadastrarCliente(Cliente cliente) {
+		try {
+			new ClienteService().salvar(cliente);
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deletarCliente(Cliente cliente) {
+		System.out.println("DEVERIA EXCLUIR");
+	}
+
+}
