a628cdd52f6cb3322908de0fef33f35ad1ba6205
CLEVERSONAVELINOFerr <CLEVERSONAVELINOFerr@9.85.199.5>
2018-06-05 21:31:16 -0300

    inclusão do swagger.

--- sistemaacademico201802/src/br/edu/up/sistemaacademico/rest/AlunoRest.java
+++ sistemaacademico201802/src/br/edu/up/sistemaacademico/rest/AlunoRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,8 +13,10 @@ import javax.ws.rs.core.MediaType;
 import br.edu.up.sistemaacademico.entity.Aluno;
 import br.edu.up.sistemaacademico.service.AlunoService;
 import br.edu.up.sistemaacademico.service.ServiceException;
+import io.swagger.annotations.Api;
 
 @Path("/aluno")
+@Api(value = "/aluno", description = "Alunos do sistema")
 public class AlunoRest {
 	
 	@GET
@@ -26,6 +29,16 @@ public class AlunoRest {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void salvar(Aluno aluno) {
+		try {
+			new AlunoService().salvar(aluno);
+		} catch (ServiceException e) {			
+			e.printStackTrace();
+		}
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void editar(Aluno aluno) {
 		try {
 			new AlunoService().salvar(aluno);
 		} catch (ServiceException e) {			
