39f2a7a85dbb60be8d6f38772be1c72531daa2a7
CLEVERSONAVELINOFerr <CLEVERSONAVELINOFerr@9.80.238.35>
2018-10-17 17:38:52 -0300

    aplicação exemplo.

---
+++ sistemaacademico201803/WebContent/main.bundle.js
@@ -0,0 +1,307 @@
+webpackJsonp(["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/aluno/aluno.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n<div class=\"container\">\n\t<h1>Cadastro de Aluno</h1>\n\t<form>\n\t\t<div class=\"form-group\">\n\t\t\t<label for=\"nome\">Nome</label> <input type=\"text\"\n\t\t\t\tclass=\"form-control\" name=\"nome\" [(ngModel)]=\"aluno.nome\" \n\t\t\t\trequired>\n\t\t</div>\n\t\t<div class=\"form-group\">\n\t\t\t<label for=\"cpf\">Matricula</label> <input type=\"text\"\n\t\t\t\tclass=\"form-control\" name=\"cpf\" [(ngModel)]=\"aluno.matricula\"\n\t\t\t\trequired>\n\t\t</div>\n\t\t<button class=\"btn btn-success\" \n\t\t*ngIf=\"aluno.id == null\" (click)=\"addAluno()\">Cadastrar</button>\n\t\t<button class=\"btn btn-success\" \n\t\t*ngIf=\"aluno.id != null\" (click)=\"updateAluno()\">Alterar</button>\n\t</form>\n\n\t<p></p>\n\t<table class=\"table\">\n\t\t<tr>\n\t\t\t<th>Nome</th>\n\t\t\t<th>Matricula</th>\n\t\t\t<th>&nbsp;</th>\n\t\t\t<th>&nbsp;</th>\n\t\t</tr>\n\t\t<tr *ngFor=\"let aluno of alunos\">\n\t\t\t<td>{{aluno.nome}} </td>\n\t\t\t<td>{{aluno.matricula}}</td>\n\t\t\t<td><button class=\"btn btn-success\" (click)=\"loadAluno(aluno)\">Carregar</button></td>\n\t\t\t<td><button class=\"btn btn-danger\" (click)=\"deleteAluno(aluno)\">Excluir</button></td>\n\t\t</tr>\n\t</table>\n\t\n</div>\n\n\n\n\n\n\n\n\n\n"
+
+/***/ }),
+
+/***/ "./src/app/aluno/aluno.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Aluno; });
+var Aluno = /** @class */ (function () {
+    function Aluno() {
+    }
+    return Aluno;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/aluno/httpaluno.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HttpAlunoComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__httpaluno_service__ = __webpack_require__("./src/app/aluno/httpaluno.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__aluno_component__ = __webpack_require__("./src/app/aluno/aluno.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var HttpAlunoComponent = /** @class */ (function () {
+    function HttpAlunoComponent(httpAlunoS) {
+        this.httpAlunoS = httpAlunoS;
+        this.aluno = new __WEBPACK_IMPORTED_MODULE_2__aluno_component__["a" /* Aluno */]();
+        this.getAlunos();
+    }
+    HttpAlunoComponent.prototype.getAlunos = function () {
+        var _this = this;
+        this.httpAlunoS.getAlunos().subscribe(function (alunos) { return _this.alunos = alunos; }, function (error) { return alert(error); }, function () { return console.log('terminou'); });
+    };
+    HttpAlunoComponent.prototype.addAluno = function () {
+        var _this = this;
+        this.httpAlunoS.addAluno(this.aluno).subscribe(function (data) { return data; }, function (error) { return alert(error); }, function () { return _this.getAlunos(); });
+    };
+    HttpAlunoComponent.prototype.updateAluno = function () {
+        var _this = this;
+        this.httpAlunoS.updateAluno(this.aluno).subscribe(function (data) { return data; }, function (error) { return alert(error); }, function () { return _this.getAlunos(); });
+    };
+    HttpAlunoComponent.prototype.deleteAluno = function (aluno) {
+        var _this = this;
+        this.httpAlunoS.deleteAluno(aluno).subscribe(function (data) { return data; }, function (error) { return alert(error); }, function () { return _this.getAlunos(); });
+    };
+    HttpAlunoComponent.prototype.loadAluno = function (aluno) {
+        this.aluno.id = aluno.id;
+        this.aluno.nome = aluno.nome;
+        this.aluno.matricula = aluno.matricula;
+    };
+    HttpAlunoComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("./src/app/aluno/aluno.component.html"),
+            providers: [__WEBPACK_IMPORTED_MODULE_1__httpaluno_service__["a" /* HttpAlunoService */]]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__httpaluno_service__["a" /* HttpAlunoService */]])
+    ], HttpAlunoComponent);
+    return HttpAlunoComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/aluno/httpaluno.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HttpAlunoService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__("./node_modules/rxjs/_esm5/add/operator/map.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_Rx__ = __webpack_require__("./node_modules/rxjs/_esm5/Rx.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+var HttpAlunoService = /** @class */ (function () {
+    function HttpAlunoService(_http) {
+        this._http = _http;
+    }
+    HttpAlunoService.prototype.getAlunos = function () {
+        return this._http.
+            get('http://localhost:8080/sistemaacademico201803/rest/alunorest').
+            map(this.extractData);
+    };
+    HttpAlunoService.prototype.extractData = function (res) {
+        return res.json();
+    };
+    HttpAlunoService.prototype.addAluno = function (aluno) {
+        var json = JSON.stringify(aluno);
+        //{"id":"1","nome":"abc"}
+        var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["a" /* Headers */]({ 'Content-Type': 'application/json' });
+        var options = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["d" /* RequestOptions */]({ headers: headers });
+        return this._http.
+            post('http://localhost:8080/sistemaacademico201803/rest/alunorest', json, options).map(function (res) { return res.json(); });
+    };
+    HttpAlunoService.prototype.updateAluno = function (aluno) {
+        var json = JSON.stringify(aluno);
+        //{"id":"1","nome":"abc"}
+        var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["a" /* Headers */]({ 'Content-Type': 'application/json' });
+        var options = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["d" /* RequestOptions */]({ headers: headers });
+        return this._http.
+            put('http://localhost:8080/sistemaacademico201803/rest/alunorest', json, options).map(function (res) { return res.json(); });
+    };
+    HttpAlunoService.prototype.deleteAluno = function (aluno) {
+        //const json = JSON.stringify(aluno);
+        //{"id":"1","nome":"abc"}
+        var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["a" /* Headers */]({ 'Content-Type': 'application/json' });
+        var options = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["d" /* RequestOptions */]({ headers: headers });
+        return this._http.
+            delete('http://localhost:8080/sistemaacademico201803' +
+            '/rest/alunorest/' + aluno.id, options).map(function (res) { return res.json(); });
+    };
+    HttpAlunoService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["A" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]])
+    ], HttpAlunoService);
+    return HttpAlunoService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-root',
+            template: "\n<a routerLink=\"/aluno\">Aluno</a>\n<router-outlet></router-outlet>"
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("./node_modules/@angular/forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_router__ = __webpack_require__("./node_modules/@angular/router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__aluno_httpaluno_component__ = __webpack_require__("./src/app/aluno/httpaluno.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__aluno_httpaluno_component__["a" /* HttpAlunoComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["c" /* HttpModule */],
+                __WEBPACK_IMPORTED_MODULE_4__angular_router__["a" /* RouterModule */].forRoot([
+                    {
+                        path: 'aluno',
+                        component: __WEBPACK_IMPORTED_MODULE_6__aluno_httpaluno_component__["a" /* HttpAlunoComponent */]
+                    }
+                ])
+            ],
+            providers: [__WEBPACK_IMPORTED_MODULE_6__aluno_httpaluno_component__["a" /* HttpAlunoComponent */]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("./node_modules/@angular/platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("./src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("./src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_13" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
